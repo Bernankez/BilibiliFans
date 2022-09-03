86e4cfdc95a7b9012c6facc12c22906a1704c45d
cole <bernankez@qq.com>
2022-09-03 23:15:23 +0800

    feat: add styles

--- src/components/FansCard.vue
+++ src/components/FansCard.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="fans-card">
-    <div class="flex flex-col justify-between h-full">
+    <img class="absolute right-0 top-0 w-[90%] object-contain" :src="Ly" />
+    <div class="background"></div>
+    <div class="relative h-full flex flex-col justify-between h-full">
       <div class="flex">
         <img class="avatar" :src="Avatar" />
         <span class="user-name">科科Cole</span>
       </div>
-      <div>
-        <div>FANS NO.</div>
-        <div>004597</div>
+      <div class="m-t-[17px]">
+        <div class="fans-mono">FANS NO.</div>
+        <div class="fans-number kenny-mini">004597</div>
       </div>
-      <div>
-        <div>DATE</div>
-        <div>2022/04/21</div>
+      <div class="m-t-[1px]">
+        <div class="fans-mono">DATE</div>
+        <div class="fans-date">2022/04/21</div>
       </div>
     </div>
   </div>
@@ -19,19 +21,35 @@
 
 <script setup lang="ts">
 import Avatar from "@/assets/avatar.jpg";
+import Ly from "@/assets/lyj.webp";
 </script>
 
 <style lang="scss" scoped>
 .fans-card {
-  padding: 14px 18px;
+  position: relative;
+  padding: 14px 18px 9px 18px;
   box-sizing: border-box;
   height: 200px;
   width: 486px;
   border-radius: 6px;
-  background-image: url("../assets/bg.jpg");
-  background-color: rgb(255, 227, 195);
   background-size: contain;
   box-shadow: 0 0 5px 2px #ccc;
+  overflow: hidden;
+}
+
+.background {
+  position: absolute;
+  z-index: 0;
+  left: 0;
+  top: 0;
+  height: 100%;
+  background: linear-gradient(to right, #333 10%, transparent 40%);
+  width: 100%;
+}
+
+@font-face {
+  font-family: "kenny mini";
+  src: url("../assets/fonts/Kenney-Mini-Square.ttf");
 }
 
 .avatar {
@@ -49,6 +67,19 @@ import Avatar from "@/assets/avatar.jpg";
 }
 
 .fans-mono {
+  @apply text-white text-[16px];
   font-family: "Google Sans Text", Arial, Helvetica, sans-serif;
+}
+
+.fans-number {
+  @apply tracking-[1px] leading-[38px] text-white text-[29px];
+}
+
+.fans-date {
+  @apply tracking-[0.4px] leading-[21px] text-[16px] text-white;
+}
+
+.kenny-mini {
+  font-family: "kenny mini";
 }
 </style>
