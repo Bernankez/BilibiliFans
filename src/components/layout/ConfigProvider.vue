be147951aa3ecc15b0632d8ea89896f6b0490365
cole <bernankez@qq.com>
2022-09-19 11:18:48 +0800

    chore: split ConfigProvider

---
+++ src/components/layout/ConfigProvider.vue
@@ -0,0 +1,13 @@
+<template>
+  <NConfigProvider :locale="zhCN" :dateLocale="dateZhCN" abstract>
+    <NMessageProvider>
+      <NDialogProvider>
+        <slot></slot>
+      </NDialogProvider>
+    </NMessageProvider>
+  </NConfigProvider>
+</template>
+
+<script setup lang="ts">
+import { NConfigProvider, NMessageProvider, NDialogProvider, zhCN, dateZhCN } from "naive-ui";
+</script>
