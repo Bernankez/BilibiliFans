cc352c8b0eda85026b9122ad41959072a0aed9de
cole <bernankez@qq.com>
2022-09-19 22:10:10 +0800

    fix: supplement

---
+++ src/components/ui/ColorSquare.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="color-square w-4 h-4 rounded-1" @click="onClick"></div>
+</template>
+
+<script setup lang="ts">
+const { color = "#ffffff", disabled = false } = defineProps<{
+  color?: string;
+  disabled?: boolean;
+}>();
+
+const emit = defineEmits<{
+  (event: "click", color: string): void;
+}>();
+
+const onClick = () => {
+  if (!disabled) {
+    emit("click", color);
+  }
+};
+
+const cursor = $computed(() => (disabled ? "not-allowed" : "pointer"));
+</script>
+
+<style lang="scss" scoped>
+.color-square {
+  background-color: v-bind("color");
+  cursor: v-bind("cursor");
+}
+</style>
