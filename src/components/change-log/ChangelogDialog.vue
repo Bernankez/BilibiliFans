4e83b007813af7051311fce27bab9a126e791384
cole <bernankez@qq.com>
2022-11-02 16:45:41 +0800

    docs: update changelog

--- src/components/change-log/ChangelogDialog.vue
+++ src/components/change-log/ChangelogDialog.vue
@@ -11,6 +11,11 @@
           target="_blank"
         >鹿野灸</a>
       </div>
+      <LogCard title="v0.3.3">
+        <LogCardItem title="Bug Fixes">
+          <div>修复自适应缩放模式下，页面垂直方向遮挡问题</div>
+        </LogCardItem>
+      </LogCard>
       <LogCard title="v0.3.2">
         <LogCardItem title="Features">
           <div>菜单栏现在可以收起了</div>
