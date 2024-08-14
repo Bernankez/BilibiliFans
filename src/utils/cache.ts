306880edd4975c4dd1e5082a5a663bfcf856385b
cole <bernankeic@gmail.com>
2024-08-14 14:06:14 +0800

    feat: lru cache for image

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -1,12 +1,21 @@
+import { LRUCache } from "lru-cache";
 import { isWebWorker } from "./is";
 
-// TODO LRU Cache
+const cache = new LRUCache<string, ImageBitmap | HTMLImageElement>({
+  max: 10,
+});
 
 export async function resolveImage(image: string, alt?: string) {
-  if (isWebWorker()) {
+  const _isWebWorker = isWebWorker();
+  const key = `${image}?${_isWebWorker ? "worker" : "browser"}`;
+  if (cache.has(key)) {
+    return cache.get(key)!;
+  }
+  if (_isWebWorker) {
     const response = await fetch(image);
     const blob = await response.blob();
     const imageBitmap = await createImageBitmap(blob);
+    cache.set(key, imageBitmap);
     return imageBitmap;
   } else {
     const img = new Image();
@@ -15,6 +24,7 @@ export async function resolveImage(image: string, alt?: string) {
       img.alt = alt;
     }
     await img.decode();
+    cache.set(key, img);
     return img;
   }
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { format, resolve } from "node:path";
+import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import UnoCSS from "unocss/vite";
