bc659fa3ada4d0aac32946111b8b40e625075ccf
cole <bernankeic@gmail.com>
2024-08-19 17:57:36 +0800

    feat: zoom cropper

--- src/components/global/Cropper.vue
+++ src/components/global/Cropper.vue
@@ -12,6 +12,8 @@ const props = withDefaults(defineProps<{
   /** w/h */
   aspectRatio?: number;
   imageRestriction?: "fit" | "none";
+  minWidth?: number;
+  minHeight?: number;
 }>(), {
   type: "rectangle",
   img: Background,
@@ -24,14 +26,6 @@ const controlledZoom = defineModel<number>("zoom");
 const uncontrolledZoom = ref(props.defaultZoom);
 
 const zoom = useMergedState(controlledZoom, uncontrolledZoom);
-
-function minus() {
-  zoom.value -= 10;
-}
-
-function plus() {
-  zoom.value += 10;
-}
 
 const stencilProps = computed(() => ({
   // lines: {},
@@ -46,7 +40,7 @@ const resizeImage = ref({
   touch: true,
   wheel: {
     ratio: 0.1,
-  },
+  } as { ratio: number } | false,
   adjustStencil: false,
 });
 
@@ -57,16 +51,79 @@ function stencilSize({ boundaries }: { boundaries: { width: number;height: numbe
     height,
   };
 }
+
+const cropperWrapperRef = ref<HTMLDivElement>();
+const cropperRef = ref<InstanceType<typeof Cropper>>();
+useResizeObserver(cropperWrapperRef, (entries) => {
+  if (entries[0]) {
+    requestAnimationFrame(refresh);
+  }
+});
+
+function refresh() {
+  cropperRef.value?.refresh();
+}
+
+function onCropperChange(e: any) {
+  if (!cropperRef.value) {
+    return;
+  }
+  const imageSize = e.image as { width: number | null; height: number | null };
+  if (!imageSize.width || !imageSize.height) {
+    return;
+  }
+  const coordinates = e.coordinates as { left: number; top: number; width: number; height: number };
+  const sizeRestrictions = cropperRef.value.sizeRestrictions as { maxHeight: number;maxWidth: number;minHeight: number;minWidth: number };
+  let _zoom: number;
+  if (imageSize.width / imageSize.height > coordinates.width / coordinates.height) {
+    _zoom = (imageSize.height - coordinates.height) / (imageSize.height - sizeRestrictions.minHeight);
+  } else {
+    _zoom = (imageSize.width - coordinates.width) / (imageSize.width - sizeRestrictions.minWidth);
+  }
+  zoom.value = _zoom * 100;
+}
+
+function minus() {
+  handleZoom((zoom.value - 10) / 100, zoom.value / 100);
+}
+
+function plus() {
+  handleZoom((zoom.value + 10) / 100, zoom.value / 100);
+}
+
+function onZoom(_zoom: number) {
+  handleZoom(_zoom / 100, zoom.value / 100);
+}
+
+function handleZoom(zoom: number, oldZoom: number) {
+  if (!cropperRef.value) {
+    return;
+  }
+  const imageSize = cropperRef.value.imageSize as { width: number | null; height: number | null };
+  if (!imageSize.height || !imageSize.width) {
+    return;
+  }
+  const sizeRestrictions = cropperRef.value.sizeRestrictions as { maxHeight: number;maxWidth: number;minHeight: number;minWidth: number };
+  if (imageSize.height < imageSize.width) {
+    const minHeight = sizeRestrictions.minHeight;
+    const imageHeight = imageSize.height;
+    cropperRef.value.zoom((imageHeight - oldZoom * (imageHeight - minHeight)) / (imageHeight - zoom * (imageHeight - minHeight)) || 0);
+  } else {
+    const minWidth = sizeRestrictions.minWidth;
+    const imageWidth = imageSize.width;
+    cropperRef.value.zoom((imageWidth - oldZoom * (imageWidth - minWidth)) / (imageWidth - zoom * (imageWidth - minWidth)) || 0);
+  }
+}
 </script>
 
 <template>
   <div class="flex flex-col gap-8">
-    <div>
-      <Cropper :debounce="0" :src="img" :stencil-props :canvas="false" :stencil-size :image-restriction="imageRestriction === 'fit' ? 'stencil' : 'none'" :resize-image :stencil-component="type === 'rectangle' ? RectangleStencil : CircleStencil" />
+    <div ref="cropperWrapperRef">
+      <Cropper ref="cropperRef" :min-width :min-height :transitions="false" :debounce="false" :src="img" :stencil-props :canvas="false" :stencil-size :image-restriction="imageRestriction === 'fit' ? 'stencil' : 'none'" :resize-image :stencil-component="type === 'rectangle' ? RectangleStencil : CircleStencil" @change="onCropperChange" />
     </div>
     <div class="flex items-center gap-4">
       <div class="i-uil-search-minus shrink-0 cursor-pointer text-lg transition hover:text-primary" @click="minus"></div>
-      <NSlider v-model:value="zoom" :tooltip="false" />
+      <NSlider :value="zoom" :tooltip="false" @update:value="onZoom" />
       <div class="i-uil-search-plus shrink-0 cursor-pointer text-2xl transition hover:text-primary" @click="plus"></div>
     </div>
   </div>
