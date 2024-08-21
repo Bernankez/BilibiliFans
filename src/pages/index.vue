077aa932c95daa253442691b6f48c88ba69c33d9
cole <bernankeic@gmail.com>
2024-08-21 16:34:21 +0800

    feat: add split window mode

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,27 +1,93 @@
 <script setup lang="ts">
+import { breakpointsTailwind } from "@vueuse/core";
 import Background from "@/assets/img/background.jpeg";
 
+const { t } = useI18n();
+const appStore = useAppStore();
+const { split, splitRatio } = storeToRefs(appStore);
+
+const { sm } = useBreakpoints(breakpointsTailwind);
+
 const nickname = ref("科科Cole");
-const lock = ref(true);
+const mode = ref<"preview" | "edit">("preview");
 
 const image = ref(Background);
 </script>
 
 <template>
   <div class="relative h-full w-full flex items-center justify-center overflow-hidden">
-    <div class="absolute right-2 top-2 z-1">
-      <Button :icon="lock ? 'i-uil-padlock' : 'i-uil-lock-open-alt'" @click="lock = !lock" />
+    <div class="absolute right-2 top-2 z-1 flex items-center gap-2 rounded-full bg-background bg-opacity-50 p-1.5 shadow transition hover:bg-opacity-100">
+      <template v-if="!split">
+        <NTooltip v-if="mode !== 'preview'" placement="bottom">
+          <template #trigger>
+            <NButton secondary circle @click="mode = 'preview'">
+              <template #icon>
+                <div class="i-material-symbols-id-card-outline-rounded"></div>
+              </template>
+            </NButton>
+          </template>
+          <span>{{ t("app.interface.togglePreview") }}</span>
+        </NTooltip>
+        <NTooltip v-if="mode !== 'edit'" placement="bottom">
+          <template #trigger>
+            <NButton secondary circle @click="mode = 'edit'">
+              <template #icon>
+                <div class="i-material-symbols-crop-rounded"></div>
+              </template>
+            </NButton>
+          </template>
+          <span>{{ t("app.interface.toggleEdit") }}</span>
+        </NTooltip>
+        <NTooltip placement="bottom">
+          <template #trigger>
+            <NButton secondary circle @click="split = true">
+              <template #icon>
+                <div :class="sm ? 'i-material-symbols-view-column-2-outline' : 'i-material-symbols-view-agenda-outline'"></div>
+              </template>
+            </NButton>
+          </template>
+          <span>{{ t("app.interface.splitWindow") }}</span>
+        </NTooltip>
+      </template>
+      <NTooltip v-else placement="bottom">
+        <template #trigger>
+          <NButton secondary circle @click="split = false">
+            <template #icon>
+              <div class="i-material-symbols-crop-7-5-outline"></div>
+            </template>
+          </NButton>
+        </template>
+        <span>{{ t("app.interface.mergeWindow") }}</span>
+      </NTooltip>
     </div>
-    <Transition name="card">
-      <div v-show="lock" class="max-w-200 w-full p-4">
-        <FansCard :nickname />
-      </div>
-    </Transition>
-    <Transition name="card">
-      <div v-show="!lock" class="h-full max-w-200 w-full flex items-center p-4">
-        <Cropper class="max-h-full w-full" :img="image" :min-width="64" :aspect-ratio="1 / 0.4115" />
-      </div>
-    </Transition>
+    <template v-if="!split">
+      <Transition name="card">
+        <div v-show="mode === 'preview'" class="max-w-200 w-full p-4">
+          <FansCard :nickname />
+        </div>
+      </Transition>
+      <Transition name="card">
+        <div v-show="mode === 'edit'" class="h-full max-w-200 w-full flex items-center p-4">
+          <Cropper class="max-h-full w-full" :img="image" :min-width="64" :aspect-ratio="1 / 0.4115" />
+        </div>
+      </Transition>
+    </template>
+    <NSplit v-else v-model:size="splitRatio" :max="0.7" :min="0.3" :direction="sm ? 'horizontal' : 'vertical'">
+      <template #1>
+        <div class="h-full flex items-center justify-center">
+          <div class="max-w-200 w-full p-4">
+            <FansCard :nickname />
+          </div>
+        </div>
+      </template>
+      <template #2>
+        <div class="h-full flex items-center justify-center">
+          <div class="h-full max-w-200 w-full flex items-center p-4">
+            <Cropper class="max-h-full w-full" :img="image" :min-width="64" :aspect-ratio="1 / 0.4115" />
+          </div>
+        </div>
+      </template>
+    </NSplit>
   </div>
 </template>
 
--- src/store/app.ts
+++ src/store/app.ts
@@ -2,9 +2,13 @@ import type { AvailableLocales } from "@/utils/i18n";
 
 export const useAppStore = defineStore("app", () => {
   const locale = ref<AvailableLocales>();
+  const split = ref(false);
+  const splitRatio = ref(0.5);
 
   return {
     locale,
+    split,
+    splitRatio,
   };
 }, {
   persist: true,
