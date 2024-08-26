3b514a0358c90675dfd5860723bea6a1ddb0d909
cole <bernankeic@gmail.com>
2024-08-26 10:51:27 +0800

    fix(Header): overflow

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -3,7 +3,7 @@ const { t } = useI18n();
 </script>
 
 <template>
-  <header class="h-[var(--header-height)] flex items-center justify-between b-b-1 b-border b-solid p-2">
+  <header class="h-[var(--header-height)] flex items-center justify-between overflow-hidden b-b-1 b-border b-solid p-2">
     <div>
     </div>
     <div class="flex items-center gap-3">
